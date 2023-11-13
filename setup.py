97e6e18b08845f051a8cedd079131eb1d83d8d90
azzammuhyala <143919179+azzammuhyala@users.noreply.github.com>
2023-11-13 20:53:58 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name         = 'asciiTUI',
-    version      = '1.0.1',
+    version      = '1.1.0',
     author       = 'azzammuhyala',
     author_email = 'azzammuhyala@gmail.com',
     description  = 'This is a library of tools for you to use with your needs for an attractive type of terminal (console) display.',
